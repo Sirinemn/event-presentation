95ea813d110231eab4bdd82e2fa5e6b03e305fb4
Sirinemn <sirinemn@outlook.fr>
2024-12-27 12:23:34 +0100

    add path routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SpeakersComponent } from './components/speakers/speakers.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'speakers', component: SpeakersComponent },
+    { path: 'contact', component: ContactComponent}
+];
