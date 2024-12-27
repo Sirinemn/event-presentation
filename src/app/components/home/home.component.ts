176a23b9fda00b5c98c8525cf80dd00b4b2ac651
Sirinemn <sirinemn@outlook.fr>
2024-12-27 12:34:00 +0100

    import LottieComponent to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LottieComponent, AnimationOptions } from 'ngx-lottie';
+import players from 'lottie-web'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    LottieComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  title = 'Evénement de Santé 2025';
+  options: AnimationOptions = {
+    path: 'asssests/animation.json'
+  };
+
+  static playerFactory() {
+    return players;
+  }
 
 }
