1bf70aaf6487d80d0cc0ba27fb9196758eeba3d4
Sirinemn <sirinemn@outlook.fr>
2024-12-27 23:50:01 +0100

    implements speakers component

--- src/app/components/speakers/speakers.component.ts
+++ src/app/components/speakers/speakers.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-speakers',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './speakers.component.html',
   styleUrl: './speakers.component.scss'
 })
 export class SpeakersComponent {
-
+  speakers = [ 
+    { name: 'Dr. John Doe', bio: 'Expert en IA et santé.', photo: 'assets/images/john-doe.png' }, 
+    { name: 'Dr. Jane Smith', bio: 'Spécialiste en technologies de la santé.', photo: 'assets/images/jane-smith.png' },
+  ];
 }
