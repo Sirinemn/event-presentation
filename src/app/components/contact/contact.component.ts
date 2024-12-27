5c845b7dc947ea73955a7a4753cb5085fb3b9a36
Sirinemn <sirinemn@outlook.fr>
2024-12-27 23:32:00 +0100

    implement contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactForm: FormGroup; 
 
+  constructor() { this.contactForm = new FormGroup({ 
+    name: new FormControl('', [Validators.required]), 
+    email: new FormControl('', [Validators.required, Validators.email]), 
+    message: new FormControl('', [Validators.required, Validators.minLength(10)]) 
+    }); 
+  } 
+  onSubmit() { 
+    if (this.contactForm.valid) { 
+      console.log('Form Submitted!', this.contactForm.value); 
+     } 
+  }
 }
