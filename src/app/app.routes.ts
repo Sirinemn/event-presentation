73c5ac7e2c7b5472cd1d47027913b27c73d6cf14
Sirinemn <sirinemn@outlook.fr>
2024-12-28 17:37:16 +0100

    add not found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { SpeakersComponent } from './components/speakers/speakers.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'speakers', component: SpeakersComponent },
-    { path: 'contact', component: ContactComponent}
+    { path: 'contact', component: ContactComponent},
+    { path: '404', component: NotFoundComponent },
+    { path: '**', redirectTo: '404' },
 ];
