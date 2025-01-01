e2dfc91707412622760080f37f16395c3db46f2d
Sirinemn <sirinemn@outlook.fr>
2025-01-02 00:01:09 +0100

    add arrow animation to home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,25 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import player from 'lottie-web'; 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  imports: [],
+  imports: [
+    RouterModule
+  ],
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit{
+  ngAfterViewInit(): void {
+    player.loadAnimation({
+      container: document.getElementById('lottie-container')!,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'assets/animation/Animation - arrow.json', 
+    });
+  }
 
 }
 
