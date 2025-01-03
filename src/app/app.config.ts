8b36c69eb2c720c006766c6d9b9779423f120cb0
Sirinemn <sirinemn@outlook.fr>
2025-01-03 22:13:34 +0100

    désinstall ngx-lottie

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import player from 'lottie-web';
-import { routes } from './app.routes';
-import { provideLottieOptions } from 'ngx-lottie';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ 
     eventCoalescing: true 
-    }),
-    provideRouter(routes),
-    provideLottieOptions({
-      player: () => player, 
     })
   ]
 };
