1495a55e0e2eba958b59bc98923534ed276fef0c
Sirinemn <sirinemn@outlook.fr>
2025-01-03 17:07:46 +0100

    add respoonsive to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import player from 'lottie-web'; 
+import gsap from 'gsap'
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -10,7 +11,17 @@ import player from 'lottie-web';
     RouterModule
   ],
 })
-export class HomeComponent implements AfterViewInit{
+export class HomeComponent implements AfterViewInit, OnInit{
+
+  ngOnInit(): void {
+    this.animateElements
+  }
+  animateElements() { 
+    gsap.from('.home h1', { duration: 1, opacity: 0, y: -50 });
+    gsap.from('.home-text', { duration: 1, opacity: 0, x: -50, stagger: 0.3 }); 
+    gsap.from('.home-image', { duration: 1, opacity: 0, x: 50, delay: 0.5 }); 
+    gsap.from('.cta-section', { duration: 1, opacity: 0, y: 50, delay: 1 }); 
+  } 
   ngAfterViewInit(): void {
     player.loadAnimation({
       container: document.getElementById('lottie-container')!,
